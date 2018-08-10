45cbfe7cdd443e6b1bc0bf1ca2210f80a32207a9
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-08-09 19:18:25 -0700

    work work

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(bodyParser.json());
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
