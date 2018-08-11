2bcace1cf27b346f7f8d2bc35615e7fcc4e6ee57
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-08-10 20:50:10 -0700

    final changes

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ app.use(bodyParser.json());
 
 // ================================================================================
 // ROUTER
-
-// Static files
-// needs to be called before the routes in order to work
-app.use(express.static('app/public'));
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 require("./app/routing/apiRoutes")(app);
