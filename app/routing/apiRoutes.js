02b31437eaf9172315187a57bbc42bda3f1e1af7
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-08-11 10:05:02 -0700

    final final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@ let path = require('path');
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on friends.js data.
 let friendsArray = require('../data/friends.js');
-console.log(friends);
+
 // ===============================================================================
 // ROUTING
 module.exports = function (app) {
